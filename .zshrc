047873ff5fc718af6888c08f41de87bc03ec6b59
SergeantO <morgueheadman@gmail.com>
2025-10-05 16:49:42 +0300

    update

--- .zshrc
+++ .zshrc
@@ -1,16 +1,27 @@
 fastfetch
 
+
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+	
 
-
-plugins=(git fzf extract history)
+plugins=(
+  git fzf extract history 
+  zsh-autosuggestions
+  zsh-syntax-highlighting
+)
 
 source /usr/share/cachyos-zsh-config/cachyos-config.zsh
+source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+
 
--- instal.sh
+++ instal.sh
@@ -4,19 +4,36 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 
+
 # copy-paste fix
 sudo pacman -S clipman
 yay -S wl-clipboard-x11
 
+
 # change shall
+cd $(mktemp -d)
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sh install.sh
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 sudo chhs /bin/zsh
 
-sudo pacman -S kitty hyprshot
-sudo pacman -S mng-look thunar code 
 
+# base desktop env  
+sudo pacman -S kitty hyprshot mng-look thunar gdm
+
+
+# gtk-theme
 git clone https://github.com/vinceliuice/Graphite-gtk-theme.git
 bash Graphite-gtk-theme/install.sh
 mng-look
+
+
+# torrent
+sudo pacman -S transmission-gtk
+xdg-mime default thunar.desktop inode/directory
+
 
 # Compiling wlogout from Source
 sudo pacman -S meson
@@ -26,8 +43,10 @@ meson build
 ninja -C build
 sudo ninja -C build install
 
-# Display managers 
-sudo pacman -S gdm
+
+# vscode
+sudo pacman -S code 
+
 
 # Docker
 sudo groupadd docker
@@ -36,28 +55,41 @@ newgrp docker
 sudo pacman -S docker docker-buildx docker-compose
 docker run hello-world
 
+
+# dev env
+sudo pacman -S jdk21-openjdk
+sudo pacman -S kotlin
+sudo pacman -S nodejs
+sudo pacman -S go
+
+
 # other
-sudo pacman -S fastfetch cmatrix telegram-desktop nodejs 
-sudo pacman -S hyprshot
+sudo pacman -S fastfetch cmatrix telegram-desktop
+
 
 # media
 sudo pacman -S vlc vlc-plugin-ffmpeg 
 
-# torrent
-sudo pacman -S transmission-gtk
-xdg-mime default thunar.desktop inode/directory
+
+# Insomnia REST Client Arch installer
+cd $(mktemp -d)
+wget https://aur.archlinux.org/cgit/aur.git/snapshot/insomnia.tar.gz
+tar xzf insomnia.tar.gz
+cd insomnia
+makepkg -s
+sudo pacman -U insomnia-*-x86_64.pkg.tar
+
 
 # install steam
 sudo pacman -S flatpak
 flatpak --user remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak --user install flathub com.valvesoftware.Steam
-
-# run
 flatpak run com.valvesoftware.Steam
+
 
 # install hearthstone
 sudo pacman -S base-devel crypto++ webkit2gtk git curl python python-virtualenv
 git clone --recursive https://github.com/0xf4b1/hearthstone-linux.git && cd hearthstone-linux
 bash hearthstone-linux/craft.sh
-cd hearthstone
-bash ./login
+bash hearthstone/login
+
